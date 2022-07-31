16fb6c1241db5cfed352780447569d986b721ea0
Nazif <nzfishrak60@gmail.com>
2022-08-01 00:50:05 +0600

    added a sticky mode feature and fixed counter

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ reps = 0
 counter =1
 cycle =0
 timer = None
+state = False #For sticky mode
 from math import floor
 from tkinter import *
 
@@ -24,6 +25,7 @@ def start_counter():
     # if reps 1,3,5... then its work time
     if  reps%2!=0:
         timer_Label.config(text="WORK",font=("Century Gothic", 40, "bold"), fg=GREEN)
+        window.attributes("-topmost", True)
         count_down(WORK_MIN*60)
     elif reps%8==0:
         timer_Label.config(text="BREAK",font=("Century Gothic", 40, "bold"), fg= RED)
@@ -43,7 +45,7 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}: {count_sec}")
     if count>0:
         global timer
-        timer = window.after(1, lambda : count_down(count-1))
+        timer = window.after(1000, lambda : count_down(count-1))
     else:
         start_counter()
         if reps %2 ==0:
@@ -70,7 +72,14 @@ def reset():
     
     
 
-
+def sticky():
+    global state
+    if state == False:
+        window.attributes("-topmost", True)
+        state = True
+    else:
+        window.attributes("-topmost", False)
+        state = False
 
 
 
@@ -78,7 +87,7 @@ def reset():
 # -----------------------UI------------------------------------------
 window = Tk()
 window.title(f"Nazif's Pomodoro Cycles Completed {cycle}")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=20, pady=20, bg=YELLOW)
 timer_Label = Label(text="Timer", fg=GREEN, font=("Century Gothic", 40, "bold"), bg=YELLOW)
 timer_Label.grid(row=0, column=1)
 tomato_img = PhotoImage(file ="tomato.png")
@@ -96,5 +105,9 @@ start_button.grid(row=2, column=0)
 reset_button = Button(text="RESET", fg="white", bg=PURPLE, highlightthickness=0,width=10,height=2, font=(FONT_NAME,10,"bold"),command=reset)
 reset_button.grid(row=2, column=2)
 check_mark = Label(text="",fg=GREEN, bg=YELLOW, highlightthickness=0, font=(FONT_NAME, 18, "bold"))
-check_mark.grid(row=3, column=1)
+check_mark.grid(row=4, column=1)
+
+
+sticky_button = Button(text="STICK", fg="white", bg=PURPLE, highlightthickness=0,width=10,height=2, font=(FONT_NAME,10,"bold"),command=sticky)
+sticky_button.grid(row=2, column=1)
 window.mainloop()
