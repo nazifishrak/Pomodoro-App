2e9201651c48a46f9ed836b949988454a572d256
Nazif <nzfishrak60@gmail.com>
2022-07-31 22:05:21 +0600

    added UI components

--- main.py
+++ main.py
@@ -13,5 +13,18 @@ from tkinter import *
 
 window = Tk()
 window.title("Nazif's Pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
+tomato_img = PhotoImage(file ="tomato.png")
+
+canvas= Canvas(width=200, height=224, bg = YELLOW, highlightthickness=0)
+canvas.create_image(100,112, image = tomato_img)
+canvas.create_text(100, 130, text=f'00:00', font=(FONT_NAME, 35, "bold"), fill = "white")
+canvas.pack()
+
+start_button = Button(text="start")
+start_button.pack(side="left")
+reset_button = Button(text="reset")
+reset_button.pack(side="right")
+
+window.mainloop()
